99c8c90941e9ce9a50bdc76538d19522d531b10c
Misko Hevery <misko@hevery.com>
2012-01-24 22:42:33 -0800

    feat(scriptTemplateLoader): provide template inlining
    populates $templateCache with content of ng-template scripts

--- test/widgetsSpec.js
+++ test/widgetsSpec.js
@@ -931,4 +931,19 @@ describe('widget', function() {
       }));
     });
   });
+
+  
+  describe('scriptTemplateLoader', function() {
+    it('should populate $templateCache with contents of a ng-template script element', inject(
+        function($compile, $templateCache) {
+          if (msie <=8) return;
+          $compile('<div>foo' +
+                     '<script id="/ignore">ignore me</script>' +
+                     '<script type="text/ng-template" id="/myTemplate.html"><x>{{y}}</x></script>' +
+                   '</div>' );
+          expect($templateCache.get('/myTemplate.html')).toBe('<x>{{y}}</x>');
+          expect($templateCache.get('/ignore')).toBeUndefined();
+        }
+    ));
+  });
 });
