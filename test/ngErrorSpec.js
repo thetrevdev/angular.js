25c093f09cf9bf1246b4a68381b6f77847eb52a0
Igor Minar <igor@angularjs.org>
2012-07-12 17:23:43 -0700

    SQUASH: clean up ngErrorSpec.js

--- test/ngErrorSpec.js
+++ test/ngErrorSpec.js
@@ -1,33 +1,38 @@
 'use strict';
 
 describe('NgError', function() {
-  it("should be of type Error", function() {
+
+  it('should be of type Error', function() {
     var myError = new NgError();
     expect(myError instanceof Error).toBeTruthy();
   });
 
-  it("should interpolate arguments", function() {
+
+  it('should interpolate arguments', function() {
     var myError = new NgError(26, 'This {0} is {1}', 'foo', 'bar');
     expect(myError.message).toEqual('This foo is bar');
   });
 
-  it("should interpolate non string arguments", function() {
+
+  it('should interpolate non-string arguments', function() {
     var foo = [1, 2, 3];
     var bar = {a: 123, b: 'baar'};
     var myError = new NgError(26, 'This {0} is {1}', foo, bar);
     expect(myError.message).toEqual('This 1,2,3 is [object Object]');
   });
 
-  it("should suppress not suppress falsy objects", function() {
+
+  it('should not suppress falsy objects', function() {
     var foo = false;
     var bar = 0;
     var myError = new NgError(26, 'This {0} is {1}', foo, bar);
     expect(myError.message).toEqual('This false is 0');
   });
 
-  it("should handle less arguments than necessary", function() {
+
+  it('should handle fewer arguments than necessary', function() {
     var foo = 'Fooooo';
     var myError = new NgError(26, 'This {0} is {1} on {2}', foo);
-    expect(myError.message).toEqual('This Fooooo is {1} on {2}');    
+    expect(myError.message).toEqual('This Fooooo is {1} on {2}');
   });
 });
